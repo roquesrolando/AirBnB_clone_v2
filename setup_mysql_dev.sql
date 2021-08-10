45f1ea0c27a3c278c6dd03864808610b814afd46
roquesrolando <roquesrolando@hotmail.com>
2021-08-10 14:23:22 +0000

    added comments

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,9 +1,15 @@
+-- Start MySQL server
+
+-- Creating databe
 CREATE DATABASE IF NOT EXISTS `hbnb_dev_db`;
 
+-- Creating user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 
+-- Granting privileges to user
 GRANT ALL PRIVILEGES ON `hbnb_dev_db`.* TO 'hbnb_dev'@'localhost';
 
+-- Grant select privileges to user
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_dev'@'localhost';
 
 FLUSH PRIVILEGES;
--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,9 +1,15 @@
+-- Start MySQL server
+
+-- Create database
 CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
 
+-- Create User
 CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 
+-- Grant all privileges to user
 GRANT ALL PRIVILEGES ON `hbnb_test_db`.* TO 'hbnb_test'@'localhost';
 
+-- Grant select privileges to user
 GRANT SELECT ON `performance_schema`.* TO 'hbnb_test'@'localhost';
 
 FLUSH PRIVILEGES;
