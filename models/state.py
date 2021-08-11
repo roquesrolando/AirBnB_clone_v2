b88862b389f63215ed2bb17c4650a6b496f9e5fa
cmdelcarmen <delcarmencaroline@gmail.com>
2021-08-11 01:53:03 +0000

    update

--- models/state.py
+++ models/state.py
@@ -4,9 +4,26 @@ from models.city import City
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from models.engine.file_storage import FileStorage
 
 class State(BaseModel):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
     cities = relationship(City, backref='state')
+
+    ''' Getter for cities '''
+    @property
+    def cities(self):
+        ''' Returns the list of City instances
+        with state_id equal to the current State.id '''
+        temp = {}
+        list_cities = []
+        temp.update(FileStorage.__objects)
+        for key, value in temp.items():
+            if key.id == BaseModel.self.id
+                if key.name != BaseModel.self.name:
+                    list_cities.append(key.name)
+        return list_cities
+
+
