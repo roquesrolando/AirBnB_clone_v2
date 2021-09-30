3f7ea30ec0afb202486f3fb3db1f17d7417688ac
roquesrolando <roquesrolando@hotmail.com>
2021-09-30 14:09:36 +0000

    missing checkers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,47 +1,52 @@
 #!/usr/bin/python3
-"""Distributes an archive to the web servers,
-using the function do_deploy:"""
-import os
-from fabric.api import *
+"""
+Fabric script based on the file 2-do_deploy_web_static.py that creates and
+distributes an archive to the web servers
+"""
 
-env.user = 'ubuntu'
-env.hosts = ['35.227.117.239', '3.89.194.105']
-env.key_filename = '~/.ssh/holberton'
+from fabric.api import env, local, put, run
+from datetime import datetime
+from os.path import exists, isdir
+env.hosts = ['142.44.167.228', '144.217.246.195']
+
+
+def do_pack():
+    """generates a tgz archive"""
+    try:
+        date = datetime.now().strftime("%Y%m%d%H%M%S")
+        if isdir("versions") is False:
+            local("mkdir versions")
+        file_name = "versions/web_static_{}.tgz".format(date)
+        local("tar -cvzf {} web_static".format(file_name))
+        return file_name
+    except:
+        return None
 
 
 def do_deploy(archive_path):
-    """Distributes an archive to the web servers and sets up the new content
-    """
-
-    if not os.path.exists(archive_path):
+    """distributes an archive to the web servers"""
+    if exists(archive_path) is False:
         return False
-    arc_name_ext = archive_path
-    if '/' in archive_path:
-        arc_name_ext = archive_path[archive_path.rindex('/') + 1:]
-    arc_name = arc_name_ext
-    if '.' in arc_name_ext:
-        arc_name = arc_name[:arc_name.rindex('.')]
+    try:
+        file_n = archive_path.split("/")[-1]
+        no_ext = file_n.split(".")[0]
+        path = "/data/web_static/releases/"
+        put(archive_path, '/tmp/')
+        run('mkdir -p {}{}/'.format(path, no_ext))
+        run('tar -xzf /tmp/{} -C {}{}/'.format(file_n, path, no_ext))
+        run('rm /tmp/{}'.format(file_n))
+        run('mv {0}{1}/web_static/* {0}{1}/'.format(path, no_ext))
+        run('rm -rf {}{}/web_static'.format(path, no_ext))
+        run('rm -rf /data/web_static/current')
+        run('ln -s {}{}/ /data/web_static/current'.format(path, no_ext))
+        return True
+    except:
+        return False
 
-    with settings(warn_only=True):
-        with cd('/tmp'):
-            result = put(archive_path, arc_name_ext)
-            if result.failed:
-                return False
 
-        new_release = '/data/web_static/releases/{}/'.format(arc_name)
-        tmp_arc = '/tmp/{}'.format(arc_name_ext)
-        current = '/data/web_static/current'
-
-        results = [
-            run('mkdir -p {}'.format(new_release)),
-            run('tar -xzf {} -C {}'.format(tmp_arc, new_release)),
-            run('rm {}'.format(tmp_arc)),
-            run('mv {}web_static/* {}'.format(new_release, new_release)),
-            run('rm -rf {}web_static'.format(new_release)),
-            run('rm {}'.format(current)),
-            run('ln -s {} {}'.format(new_release, current))
-            ]
-        if all(list(map(lambda r: r.succeeded, results))):
-            print("New version Deployed!   0 errors")
-            return True
-    return False
+def deploy():
+    """creates and distributes an archive to the web servers"""
+    archive_path = do_pack()
+    if archive_path is None:
+        return False
+    return do_deploy(archive_path)
